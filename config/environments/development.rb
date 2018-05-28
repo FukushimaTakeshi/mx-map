a7adebb9b360e6d0e54cd297f2e130f9261a2e94
FukushimaTakeshi <whippet.818@gmail.com>
2018-05-28 20:23:55 +0900

    uninstall spring

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::FileUpdateChecker
+  BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
 end
