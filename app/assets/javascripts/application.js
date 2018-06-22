4c419082308bf8a839ea38b201552fcf76d413af
FukushimaTakeshi <whippet.818@gmail.com>
2018-06-22 20:00:36 +0900

    Refactor HTML & CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,19 +1,12 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require @fortawesome/fontawesome
+//= require @fortawesome/fontawesome-free-solid
+//= require @fortawesome/fontawesome-free-regular
+//= require @fortawesome/fontawesome-free-brands
 //= require_tree .
+
 document.addEventListener('turbolinks:load', function () {
 
   // Get all "navbar-burger" elements
