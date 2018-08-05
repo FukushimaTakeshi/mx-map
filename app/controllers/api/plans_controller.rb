d63ebd43ecf9f9081aa7cab7557f1d1e42c1d17e
FukushimaTakeshi <whippet.818@gmail.com>
2018-08-05 18:31:30 +0900

    Refactor plans api

--- app/controllers/api/plans_controller.rb
+++ app/controllers/api/plans_controller.rb
@@ -2,26 +2,17 @@ module Api
   class PlansController < ActionController::Base
     def index
       if params[:all]
-        plans = Plan.where(date: params[:date], off_road_circuit_id: params[:off_road_circuit_id]).count
+        plans = Plan.search_circuit_id(params[:off_road_circuit_id]).search_date(params[:date]).search_user_id(params[:user_id]).count
       else
-        plans = Plan.includes(:user).references(:user).where(
-                  plans: {
-                    off_road_circuit_id: params[:off_road_circuit_id], date: params[:date]
-                  }
-                )
-        users = plans.map do |plan|
-          if plan.user.present?
-            { id: plan.user.id, username: plan.user.username, avatar: plan.user.avatar }
-          else
-            { id: nil, username: "ゲストユーザ" }
-          end
-        end
+        plans = Plan.with_user.search_circuit_id(params[:off_road_circuit_id]).search_date(params[:date]).search_user_id(params[:user_id])
+        users = user_list(plans)
       end
       id = if current_user
-             Plan.find_by(date: params[:date], off_road_circuit_id: params[:off_road_circuit_id], user_id: current_user.id)&.id
+             Plan.search_circuit_id(params[:off_road_circuit_id]).search_date(params[:date]).find_by(user_id: current_user.id)&.id
            else
-             Plan.find_by(date: params[:date], off_road_circuit_id: params[:off_road_circuit_id], uuid: session['session_id'])&.id
+             Plan.search_circuit_id(params[:off_road_circuit_id]).search_date(params[:date]).find_by(uuid: session['session_id'])&.id
            end
+
       render json: {
         plans: plans,
         users: users,
@@ -46,6 +37,16 @@ module Api
         :date,
         :off_road_circuit_id
       )
+    end
+
+    def user_list(plans)
+      plans.map do |plan|
+        if plan.user.present?
+          { id: plan.user.id, username: plan.user.username, avatar: plan.user.avatar&.url }
+        else
+          { id: nil, username: "ゲストユーザ" }
+        end
+      end
     end
   end
 end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -12,4 +12,13 @@
 class Plan < ApplicationRecord
   belongs_to :off_road_circuit
   belongs_to :user, optional: true
+
+  scope :with_user, -> { includes(:user).references(:user) }
+  scope :search_circuit_id, lambda { |id| where(off_road_circuit_id: id) }
+  scope :search_date, lambda { |date|
+    where(date: date) if date.present?
+  }
+  scope :search_user_id, lambda { |user_id|
+    where(user_id: user_id) if user_id.present?
+  }
 end
