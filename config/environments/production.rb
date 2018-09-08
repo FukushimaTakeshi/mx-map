a0a1e55391aecd20edf922403a7978eba5ca9f6b
FukushimaTakeshi <whippet.818@gmail.com>
2018-09-08 20:51:34 +0900

    production setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,11 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  config.cache_store = :redis_store, ENV['REDISCLOUD_URL']
+  config.cache_store = :redis_store, {
+    url: 'redis://localhost',
+    port: 6379,
+    db: 1
+  }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
