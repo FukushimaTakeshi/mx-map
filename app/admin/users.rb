bdc4e602945dcc8fae1349eaae6f0737442bd5dd
FukushimaTakeshi <whippet.818@gmail.com>
2018-10-03 18:41:28 +0900

    update active admin

---
+++ app/admin/users.rb
@@ -0,0 +1,3 @@
+ActiveAdmin.register User do
+  permit_params :confirmed_at, :admin
+end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -200,6 +200,11 @@ ActiveAdmin.setup do |config|
   #
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
+  config.clear_stylesheets!
+  config.register_stylesheet 'active_admin.css'
+
+  config.clear_javascripts!
+  config.register_javascript 'active_admin.js'
 
   # == CSV options
   #
