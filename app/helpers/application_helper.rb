e36355593c55e3435d0d855746979d340860f08e
FukushimaTakeshi <whippet.818@gmail.com>
2018-11-05 00:24:33 +0900

    Implement meta-tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site:        Settings.meta.name,
+      charset:     'utf-8',
+      reverse:     true,
+      title:       Settings.meta.title,
+      description: Settings.meta.description,
+      keywords:    Settings.meta.keywords,
+      canonical:   request.original_url,
+      index:       true,
+      follow:      true,
+      icon: [
+        { href: image_url('favicon.ico') },
+        # { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        title:       :title,
+        type:        Settings.meta.og.type,
+        url:         request.original_url,
+        # image:       '',
+        site_name:   :site,
+        description: :description,
+        locale:      'ja_JP'
+      }
+    }
+  end
 end
