6677ecfb6109700d4b52baeacf5d470c1171cc6a
FukushimaTakeshi <whippet.818@gmail.com>
2018-11-05 21:39:56 +0900

    Fix user authenticate action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:show, :edit, :update]
-  before_action :correct_user, only: [:edit, :update]
-
-  def index
-    @users = User.all
-  end
+  before_action :authenticate_user!
+  before_action :correct_user
 
   def show
     @user = User.find(params[:id])
@@ -34,14 +30,7 @@ class UsersController < ApplicationController
     )
   end
 
-  def logged_in_user
-    unless user_signed_in?
-      flash[:notice] = 'ログインまたは登録が必要です。'
-      redirect_to new_user_session_url
-    end
-  end
-
   def correct_user
-    redirect_to root_url unless current_user.id.to_s == params[:id]
+    redirect_to user_path(current_user) unless current_user.id.to_s == params[:id]
   end
 end
