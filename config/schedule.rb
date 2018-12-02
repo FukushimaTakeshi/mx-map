98e78f92055ba399c454b5c884a6807d100d0d55
FukushimaTakeshi <whippet.818@gmail.com>
2018-12-02 19:29:01 +0900

    update whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,9 @@ set :output, "#{Rails.root}/log/cron_log.log"
 rails_env = ENV['RAILS_ENV'] || :development
 set :environment, rails_env
 
+env :PATH, ENV['PATH']
+job_type :rbenv_rake, %q!eval "$(rbenv init -)"; cd :path && :environment_variable=:environment bundle exec rake :task --silent :output!
+
 every :sunday, at: '9am' do
   rake 'chache_contents:place_details_search'
 end
