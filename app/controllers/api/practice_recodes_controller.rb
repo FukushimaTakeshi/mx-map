f5f0bfc1f5a8c98142196871bf4d9bb5e0427b1e
FukushimaTakeshi <whippet.818@gmail.com>
2019-01-20 18:33:55 +0900

    Implement practice_recodes to api

---
+++ app/controllers/api/practice_recodes_controller.rb
@@ -0,0 +1,41 @@
+class Api::PracticeRecodesController < ActionController::API
+  before_action :authenticate_user!
+
+  def index
+    render json: PracticeRecode.where(user_id: current_user.id)
+  end
+
+  def show
+    render json: PracticeRecode.find(params[:id])
+  end
+
+  def create
+    practice_recode = PracticeRecode.new(practice_recode_params)
+    if practice_recode.save
+      head :ok
+    else
+      render json: practice_recode.errors.full_messages
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def practice_recode_params
+    params.require(:practice_recode).permit(
+      :practice_date,
+      :off_road_circuit_id,
+      :duration_hour,
+      :duration_minute,
+      :comment
+    ).merge(user_id: current_user.id)
+  end
+end
